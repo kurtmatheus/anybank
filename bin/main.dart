e477eb8311b4d2e8fad53405fcc4c7beb7c57ba4
matheus.sampaio1 <matheus.sampaio@prodigasistemas.com.br>
2025-05-13 12:56:30 -0300

    [DEV - Asyn Rest API] Aula 3

--- bin/main.dart
+++ bin/main.dart
@@ -1,23 +1,27 @@
 import 'dart:convert';
 
+import 'package:anybank/api_key.dart';
 import 'package:http/http.dart';
 
 void main() {
   print("Olá, Mundo!");
-  String url =
-      "https://gist.githubusercontent.com/kurtmatheus/52403cf53a5ce5a94e6b9529d447c3ef/raw/a99d5e2dba57d495f88ee4b3a0a28809a2d312be/gistfile1.txt";
+  // String url =
+  //     "https://gist.githubusercontent.com/kurtmatheus/52403cf53a5ce5a94e6b9529d447c3ef/raw/a99d5e2dba57d495f88ee4b3a0a28809a2d312be/gistfile1.txt";
   // requestData(url);
-  // requestDataAsync(url)
+  // requestDataAsync(url);
+
+  String urlPost =
+      "https://api.github.com/gist/52403cf53a5ce5a94e6b9529d447c3ef";
+
   sendDataAsync({
     "id": "NEW001",
     "firstName": "Kurt",
     "lastName": "Flutter",
-    "balance": 5000
-  }, url);
+    "balance": 5000,
+  }, urlPost);
 }
 
 requestData(String url) {
-  
   Future<Response> futureResponse = get(Uri.parse(url));
   print(futureResponse);
   futureResponse.then((Response response) {
@@ -25,7 +29,9 @@ requestData(String url) {
     // print(response.body);
 
     List<dynamic> listAccounts = json.decode(response.body);
-    Map<String, dynamic> mapCarla = listAccounts.firstWhere((e) => e["name"] == "Carla");
+    Map<String, dynamic> mapCarla = listAccounts.firstWhere(
+      (e) => e["name"] == "Carla",
+    );
     print(mapCarla["balance"]);
   });
 }
@@ -39,5 +45,22 @@ sendDataAsync(Map<String, dynamic> mapAccount, String url) async {
   List<dynamic> accounts = await requestDataAsync(url);
   accounts.add(mapAccount);
   String content = json.encode(accounts);
-  print(content);
+
+  Response response = await post(
+    Uri.parse(url),
+    headers: {
+      "Authorization": "Bearer $githubApiKey",
+      "Accept": "application/vnd.github+json",
+      "X-GitHub-Api-Version": "2022-11-28"
+      },
+    body: json.encode({
+      "description": "account.json",
+      "public": true,
+      "files": {
+        "accounst.json": {"content": content},
+      },
+    }),
+  );
+
+  print(response.statusCode);
 }
